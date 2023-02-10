ccd43b76f5fa27e46b60b095a603fbbf3272b344
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-10 17:20:19 -0500

    create base sketcher

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const box = document.getElementById('box');
+const boxContainer = document.getElementById('box-container')
+const elements = document.querySelectorAll('.box');
+console.log(elements);
+appendNCopies(6400, box,boxContainer);
+
+
+(document.querySelectorAll('.box')).forEach((item) => {
+    item.addEventListener('mouseover', ()=> {
+        item.setAttribute('style', 'background: black;')
+        item.style.borderColor = "black"
+    })
+});
+
+createVar();
+
+//function to create a variable for each box
+function createVar() {
+    for( let i =0; i < 255; i++) {
+    let variable =  document.getElementById('box'+i);
+    variable = box + i ;
+}}
+//add event listener for mouseover for id of box.
+
+//create loop that will add event listeners to each of the boxes
+
+function appendNCopies(n, original, appendTo){
+    for(let i = 0; i < n; i++){
+        var clone = original.cloneNode(true);
+        clone.setAttribute('class','box');
+        clone.setAttribute('id', 'box' + i)
+        appendTo.appendChild(clone);   
+    }
+}
+
