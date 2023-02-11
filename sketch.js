7458148926e7664b3715bd72cefc6ae8fa019101
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-11 08:36:03 -0500

    fix bug

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const elements = document.querySelectorAll('.box');
 console.log(elements);
 appendNCopies(6400, box,boxContainer);
 
-
+//add event listener for mouseover for id of box.
 (document.querySelectorAll('.box')).forEach((item) => {
     item.addEventListener('mouseover', ()=> {
         item.setAttribute('style', 'background: black;')
@@ -14,13 +14,13 @@ appendNCopies(6400, box,boxContainer);
 
 createVar();
 
-//function to create a variable for each box
+//function to create a variable for each box with a new id for each
 function createVar() {
     for( let i =0; i < 255; i++) {
     let variable =  document.getElementById('box'+i);
     variable = box + i ;
 }}
-//add event listener for mouseover for id of box.
+
 
 //create loop that will add event listeners to each of the boxes
 
