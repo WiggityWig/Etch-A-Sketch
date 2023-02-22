5e99633e0dcd9dd4be196cc7795e98ed7e15a479
WiggityWig <122560716+WiggityWig@users.noreply.github.com>
2023-02-22 09:34:35 -0500

    add erase and color mode

--- sketch.js
+++ sketch.js
@@ -1,35 +1,67 @@
 const box = document.getElementById('box');
-const boxContainer = document.getElementById('box-container')
-const elements = document.querySelectorAll('.box');
-console.log(elements);
-appendNCopies(2500, box,boxContainer);
+const boxContainer = document.getElementById('box-container');
 
-//add event listener for mouseover for id of box.
-(document.querySelectorAll('.box')).forEach((item) => {
-    item.addEventListener('mouseover', ()=> {
-        item.setAttribute('style', 'background: black;')
-        item.style.borderColor = "black"
-    })
+// Set the initial mode to color
+let mode = 'color';
+
+// Get the color and eraser buttons
+const colorButton = document.getElementById('color');
+const eraserButton = document.getElementById('eraser');
+
+// Add event listeners to the buttons to switch between modes
+colorButton.addEventListener('click', () => {
+  mode = 'color';
 });
 
-createVar();
+eraserButton.addEventListener('click', () => {
+  mode = 'eraser';
+});
 
-//function to create a variable for each box with a new id for each
-function createVar() {
-    for( let i =0; i < 255; i++) {
-    let variable =  document.getElementById('box'+i);
-    variable = box + i ;
-}}
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', updateBoxCount);
 
+const rangeSlider = document.getElementById('range-slider');
+rangeSlider.addEventListener('input', updateBoxCount);
 
-//create loop that will add event listeners to each of the boxes
+// Function to update the number of boxes
+function updateBoxCount() {
+  const numBoxes = rangeSlider.value;
+  const boxSize = 500 / numBoxes;
+  const boxContainer = document.getElementById('box-container');
 
-function appendNCopies(n, original, appendTo){
-    for(let i = 0; i < n; i++){
-        var clone = original.cloneNode(true);
-        clone.setAttribute('class','box');
-        clone.setAttribute('id', 'box' + i)
-        appendTo.appendChild(clone);   
-    }
+  boxContainer.innerHTML = '';
+
+  for (let i = 0; i < numBoxes ** 2; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.setAttribute('id', 'box' + i);
+    boxContainer.appendChild(box);
+  }
+
+  // Update the box dimensions element
+  const boxDimensions = document.getElementById('box-dimensions');
+  boxDimensions.textContent = `${numBoxes} x ${numBoxes}`;
+
+  // Add event listener to each box element
+  const elements = document.querySelectorAll('.box');
+  let opacities = {};
+  elements.forEach((el) => {
+    opacities[el.id] = 0;
+    el.addEventListener('mouseover', () => {
+      const id = el.id;
+      if (mode === 'color') {
+        opacities[id] += 0.1;}
+        else if (mode === 'eraser'){
+            opacities[id] -= 0.1;
+        };
+      el.style.backgroundColor = `rgba(0, 0, 0, ${opacities[id]})`;
+    });
+  });
 }
 
+// Add event listener to update the number of boxes on page load
+window.addEventListener('DOMContentLoaded', () => {
+  updateBoxCount();
+});
